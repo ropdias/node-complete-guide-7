ff4dbdfbaaf9ef5accfb419a37dba67266445bf6
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2022-07-22 17:28:13 -0300

    Initial version

---
+++ README.md
@@ -0,0 +1,21 @@
+# Node.js: SQL Introduction
+
+Examples from the **"Section 10: SQL Introduction"** of the course **"NodeJS - The Complete Guide (MVC, REST APIs, GraphQL, Deno)"** by Academind (Maximilian Schwarzmüller).
+
+Section 10 covered:
+
+- [x] Choosing a Database
+- [x] NoSQL Introduction
+- [x] Comparing SQL and NoSQL
+- [x] Setting Up MySQL
+- [ ] Connecting our App to the SQL Database
+- [ ] Basic SQL & Creating a Table
+- [ ] Retrieving Data
+- [ ] Fetching Products
+- [ ] Fetching Products - Time to Practice
+- [ ] Inserting Data Into the Database
+- [ ] Fetching a Single Product with the "where" Condition
+
+
+
+More info: https://www.udemy.com/course/nodejs-the-complete-guide/
---
+++ controllers/admin.js
@@ -0,0 +1,80 @@
+const Product = require("../models/product");
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+
+  const product = new Product(null, title, imageUrl, description, price);
+  product.save();
+  res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+  // We are already coming from a 'edit-product' route but this was added just to show
+  // how to retrieve data from a query param:
+  const editMode = req.query.edit;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    // If we don't have a product and it's undefined:
+    if (!product) {
+      // We could retrieve a error page (better user experience) but for now we will just redirect:
+      return res.redirect("/");
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product,
+    });
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  // Fetch information from the product
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  // Create a new Product instance and populate with that information
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+  );
+  // Call the .save() method
+  updatedProduct.save();
+  res.redirect("/admin/products");
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/admin/products",
+    });
+  });
+};
+
+exports.postDeleteProduct = (req, res, next) => {
+  // Fetch information from the product
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect("/admin/products");
+};
